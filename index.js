8515e9c547f707f563b75f2ea3c03956db26833f
Tom <tmelliottjr@gmail.com>
2017-12-31 10:40:44 -0500

    properly throw error on invalid input, update tests

--- index.js
+++ index.js
@@ -2,9 +2,29 @@ module.exports = (function () {
   const base = 62;
   const alpha = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 
-  function encode(id, cb) {
-    if (typeof id !== 'number' || Number.isNaN(id)) {
-      cb(new Error(`Invalid Input. Expected 'number', received '${Number.isNaN(id) ? 'NaN' : typeof id}'`));
+  function InvalidInputException(expected, actual) {
+    this.expected = expected;
+    this.actual = actual;
+    this.toString = function () {
+      return `Expected type '${this.expected}', received typed '${this.actual}' instead.`;
+    };
+  }
+
+  InvalidInputException.prototype = Object.create(Error.prototype);
+  InvalidInputException.prototype.name = 'InvalidInputException';
+
+  function encode(id) {
+    if (typeof id !== 'number' || Number.isNaN(id) || !Number.isInteger(id)) {
+      let type = '';
+      if (Number.isNaN(id)) {
+        type = 'NaN';
+      } else if (typeof id === 'number' && !Number.isInteger(id)) {
+        type = `${typeof id} non-integer`;
+      } else {
+        type = typeof id;
+      }
+
+      throw new InvalidInputException('number', type);
     } else {
       let remainder = 0;
       let result = '';
@@ -16,13 +36,13 @@ module.exports = (function () {
         result += alpha.charAt(remainder);
       }
 
-      cb(null, result);
+      return result;
     }
   }
 
-  function decode(str, cb) {
+  function decode(str) {
     if (typeof str !== 'string') {
-      cb(new Error(`Invalid Input. Expected 'string', received '${typeof str}'`));
+      throw new InvalidInputException('string', typeof str);
     } else {
       let result = 0;
 
@@ -30,7 +50,7 @@ module.exports = (function () {
         result = (result * base) + alpha.indexOf(str.charAt(i));
       }
 
-      cb(null, result);
+      return result;
     }
   }
 
--- test/test.js
+++ test/test.js
@@ -7,21 +7,39 @@ describe('Module', () => {
     urlShortener = require('../index.js');
     assert(urlShortener !== 'undefined');
     done();
-  })
+  });
 });
 
 describe('Encode/Decode', () => {
   it('should encode correctly', (done) => {
-    urlShortener.encode(123456789, (err, res) => {
-      assert.equal(res, 'Xk0M8');
-      done();
-    });
+    result = urlShortener.encode(123456789);
+    assert.equal(result, 'Xk0M8');
+    done();
   });
 
   it('should decode correctly', (done) => {
-    urlShortener.decode('Xk0M8', (err, res) => {
-      assert.equal(res, 123456789);
+    const result = urlShortener.decode('Xk0M8');
+    assert.equal(result, 123456789);
+    done();
+  });
+
+  it('encode should throw on non integer input', (done) => {
+    let result = '';
+    try {
+      result = urlShortener.encode(123456789.10);
+    } catch (e) {
+      assert(e);
       done();
-    });
+    }
+  });
+
+  it('decode should throw on non string input', (done) => {
+    let result = '';
+    try {
+      result = urlShortener.decode({ value: 'Xk0M8' });
+    } catch (e) {
+      assert(e);
+      done();
+    }
   });
 });
